17029e7b71b205243660c70577c0127cb61993d9
Fengdexiu <91536096+Fengdexiu@users.noreply.github.com>
2021-10-05 16:12:33 +0800

    2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,7 @@
 /*
-	Dimension by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
+	WebPage by David
+    Hello!This Is A Personal WebPage.Welcome!
+ */   
 (function($) {
 
 	var	$window = $(window),
